216e6ee3648fbcb906c91a799e8674f27b1c2afa
Thanakorn Amnuaywiboolpol <l3irdWings@gmail.com>
2018-02-14 15:46:07 +0700

    Add get from download url

--- FirebaseStorageCache/FirebaseStorageCache.swift
+++ FirebaseStorageCache/FirebaseStorageCache.swift
@@ -59,6 +59,40 @@ public class FirebaseStorageCache {
         })
     }
     
+    public func get(url: URL, completion: @escaping (_ object: Data?) -> Void) {
+        
+        let storageReference = Storage.storage().reference(forURL: url.absoluteString)
+        let filePath = self.filePath(storageReference: storageReference)
+        
+        cache.get(key: filePath, completion: { object in
+            if let object = object {
+                // Cache hit
+                DispatchQueue.main.async(execute: {
+                    completion(object)
+                })
+                return
+            }
+            // Cache miss: download file
+            URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
+                guard let httpURLResponse = response as? HTTPURLResponse,
+                    httpURLResponse.statusCode == 200,
+                    let data = data, error == nil else {
+                        print(error?.localizedDescription ?? "Error status code \(String(describing: (response as? HTTPURLResponse)?.statusCode))")
+                        DispatchQueue.main.async(execute: {
+                            completion(nil)
+                        })
+                        return
+                }
+                // Store result in cache
+                self.cache.add(key: filePath, data: data, completion: {
+                    DispatchQueue.main.async(execute: {
+                        completion(data)
+                    })
+                })
+            }).resume()
+        })
+    }
+    
     public func remove(storageReference: StorageReference) {
         cache.remove(key: filePath(storageReference: storageReference), completion: nil)
     }
